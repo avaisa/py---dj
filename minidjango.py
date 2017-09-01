a93786dbd434a34fc8a9697aeaa86c8b52623204
ibin-xinzong <avaisa@163.com>
2017-09-01 10:48:35 +0800

    init

--- minidjango.py
+++ minidjango.py
@@ -1,5 +1,11 @@
 #coding=utf-8
 #base django
-import django
-print django.VERSION
+import os
+import sys
+from django.conf import settings
+DEBUG=os.environ.get('DEBUG','on')=='on'
+
+SECRET_KEY=os.environ.get('SECRET_KEY','{{secret_key}}')
+ALLOWED_HOST=os.environ.get("ALLOWED_HOST","localhost").split(',')
+
 
